5acce04fef5cbe07fb5e45bb3c655757cda38199
xutiancheng <xutiancheng@qgutech.com>
2019-08-28 08:29:51 +0800

    集合

--- git-test/src/main/java/First.java
+++ git-test/src/main/java/First.java
@@ -1,12 +1,25 @@
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class First {
     public static void main(String[] args) {
-        System.out.println("123");
-        System.out.println(1231313);
-        System.out.println(343443);
-        System.out.println(343443);
-        System.out.println(343443);
-        System.out.println(343443);
-        System.out.println(343443);
-        System.out.println("ahhahahah");
+        User user1 = new User("张三", "男", 22);
+        User user2 = new User("李四", "女", 19);
+        User user3 = new User("王五", "男", 20);
+        User user4 = new User("赵六", "男", 25);
+        User user5 = new User("小红", "女", 18);
+        User user6 = new User("小明", "男", 21);
+        ArrayList<User> userList = new ArrayList<User>(6);
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+        userList.add(user4);
+        userList.add(user5);
+        userList.add(user6);
+        System.out.println(userList);
+
+        System.out.println(userList.stream().map(User::getName).collect(Collectors.toList()));
+        System.out.println(userList.stream().map(User::getAge).collect(Collectors.toList()));
+        System.out.println(userList.stream().map(User::getSex).collect(Collectors.toList()));
     }
 }
